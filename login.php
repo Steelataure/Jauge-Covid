493ff473bf22cd90006bd39300c445b465980e0f
Steel <59340020+Steelataure@users.noreply.github.com>
2022-04-29 23:22:05 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,51 +1,39 @@
 <?php
-// Initialize the session
 session_start();
  
-// Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: admin.php");
     exit;
 }
  
-// Include config file
 require_once "config.php";
-// Define variables and initialize with empty values
+
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
  
-// Processing form data when form is submitted
 if(($_SERVER["REQUEST_METHOD"] ?? 'GET') == "POST"){
  
-    // Check if username is empty
     if(empty(trim($_POST["username"]))){
         $username_err = "Entrez un nom d'utilisateur";
     } else{
         $username = trim($_POST["username"]);
     }
     
-    // Check if password is empty
     if(empty(trim($_POST["password"]))){
         $password_err = "Entrez un mot de passe";
     } else{
         $password = trim($_POST["password"]);
     }
     
-    // Validate credentials
     if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = :username";
         
         if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
             
-            // Set parameters
             $param_username = trim($_POST["username"]);
             
-            // Attempt to execute the prepared statement
             if($stmt->execute()){
-                // Check if username exists, if yes then verify password
                 if($stmt->rowCount() == 1){
                     if($row = $stmt->fetch()){
                         $id = $row["id"];
@@ -53,36 +41,29 @@ if(($_SERVER["REQUEST_METHOD"] ?? 'GET') == "POST"){
                         $hashed_password = $row["password"];
 
                         if($password == $hashed_password){
-                            // Password is correct, so start a new session
                             session_start();
                             
-                            // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;                            
                             
-                            // Redirect user to welcome page
                             header("location: admin.php");
 
                         } else{
-                            // Password is not valid, display a generic error message
                             $login_err = "Nom d'utilisateur ou mot de passe invalide";
                         }
                     }
                 } else{
-                    // Username doesn't exist, display a generic error message
                     $login_err = "Nom d'utilisateur ou mot de passe invalide";
                 }
             } else{
                 echo "Problèmes techniques...";
             }
 
-            // Close statement
             unset($stmt);
         }
     }
     
-    // Close connection
     unset($pdo);
 }
 ?>
